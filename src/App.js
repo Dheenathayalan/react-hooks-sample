d4f711ab14547ccdf26db70e93f1573607a2a3b9
Dheenathayalan <dheenathayalan10@gmail.com>
2019-03-18 20:25:26 +0530

    Simple todo app using react hooks

--- src/App.js
+++ src/App.js
@@ -1,28 +1,48 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useState, useReducer } from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+export default function () {
+  const [name, setName] = useState('');
+  const [state, dispatch] = useReducer((state, action) => {
+    switch (action.type) {
+      case 'ADD':
+        return state;
+      case 'FETCH':
+        return state;
+      default:
+        return state;
+    }
+  }, {
+      name: '',
+      namesList: []
+    })
 
-export default App;
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  }
+
+  const handleCreateTask = (e) => {
+    state.namesList.push(name);
+    setName('');
+    dispatch({ type: 'ADD' });
+    e.preventDefault();
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          <strong className="Title">TODO</strong>
+          <form onSubmit={handleCreateTask}>
+            <input value={name} onChange={handleNameChange} autoFocus />
+            <button>Add</button>
+          </form>
+          {state.namesList.map((name, index) => (
+            <div key={index}>{name}</div>
+          ))
+          }
+        </div>
+      </header>
+    </div>
+  );
+}
